48dd8919d2a30de71a0dbb2ba6f761738bf27ea3
Abhyanand <86824669+OneFriendlyCoder@users.noreply.github.com>
2023-11-01 11:57:52 +0530

    useState usecase 1

--- src/useState/StateTutorial.js
+++ src/useState/StateTutorial.js
@@ -3,15 +3,28 @@ import React, {useState} from 'react'
 const StateTutorial = () => {
     
     const [counter, setCounter] = useState(0);
-
     const increment = () => {
         setCounter(counter+1);
     }
+
+    const [inputValue, setInputValue] = useState("Helloworld");
+    let handleChange = (event) => {
+        const newValue = event.target.value;
+        setInputValue(newValue);   
+    }
+
     return (
-    <div> 
-        { counter }
-        <button onClick={increment}>Increment value</button> 
-    </div>
+    <>
+        <div> 
+            { counter }
+            <button onClick={increment}>Increment value</button> 
+        </div>  
+
+        <div>
+            <input placeholder='enter something' onChange={handleChange}/>{inputValue}
+        </div>  
+    </>
+
     )
 };
 
