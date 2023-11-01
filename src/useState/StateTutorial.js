59081b2ab19d6558e4817556f6257f9ef58dd816
Abhyanand <86824669+OneFriendlyCoder@users.noreply.github.com>
2023-11-01 12:15:06 +0530

    useState updating form

--- src/useState/StateTutorial.js
+++ src/useState/StateTutorial.js
@@ -13,6 +13,13 @@ const StateTutorial = () => {
         setInputValue(newValue);   
     }
 
+// UPDAING OBJECTS AND ARRAYS IN STATE
+    const [form, setForm] = useState({
+        firstName : "Hello",
+        lastName : "world",
+        email: "helloworld@gmail.com"
+    });
+
     return (
     <>
         <div> 
@@ -23,6 +30,24 @@ const StateTutorial = () => {
         <div>
             <input placeholder='enter something' onChange={handleChange}/>{inputValue}
         </div>  
+
+        <div>
+            <label>
+                First name: <input value={form.firstName} onChange={e => {setForm({...form, firstName: e.target.value});}}/>
+            </label>
+            <label>
+                Last name: <input value={form.lastName} onChange={e => {setForm({...form, lastName: e.target.value});}}/>
+            </label>
+            <label>
+                Email: <input value={form.email} onChange={e => {setForm({...form, email: e.target.value});}}/>
+            </label>
+            <p>
+                {form.firstName}{' '}
+                {form.lastName}{' '}
+                {form.email}
+            </p>
+        </div>
+
     </>
 
     )
